047972f0ed73bf5eaffe9ba369bcaeac24dca13d
Carter Hemington <carter.hemington@gmail.com>
2022-02-02 01:37:20 +0000

    added renderTweet and createTweetElement functions - tested as well

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,81 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  console.log("DOM is ready to be manipulated")
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    //loops through tweets
+    for (const tweet of tweets) {
+       //calls createTweetElement for each tweet
+       $('.tweetContainer').prepend(createTweetElement(tweet));
+       //takes return value and appends it to each tweet container
+    }
+    
+  }
+
+  const createTweetElement = function(tweet) {
+    const $tweet = `
+    <article class="tweet">
+          <header class="tweetHeader">
+            <div class="tweetPoster">
+            <img src="${tweet.user.avatars}" />
+              <p>${tweet.user.name}</p>
+            </div>
+            <address>${tweet.user.handle}</address>
+          </header>
+          <div class="textInTweet">
+            <h5>
+             ${tweet.content.text}
+            </h5>
+          </div>
+          <footer class="tweetFooter">
+            <p> ${timeago.format(tweet.created_at)}</p>
+            <div class="icons">
+              <a
+                ><i class="fas fa-flag"></i><i class="fas fa-retweet"></i
+                ><i class="fas fa-heart"></i
+              ></a>
+            </div>
+          </footer>
+        </article>
+    `
+    
+    return $tweet
+  }
+  // Test / driver code (temporary). Eventually will get this from the server.
+  
+
+// const $tweet = createTweetElement(data);
+
+// Test / driver code (temporary)
+// console.log($tweet); // to see what it looks like
+// $('.tweetContainer').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+renderTweets(data);
+});
